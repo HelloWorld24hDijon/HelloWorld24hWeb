a5f348f33a58019b59188a71f15a16d19649d374
BrightHammer21 <lucieboucquemont@hotmail.fr>
2018-05-26 05:08:16 +0200

    e

--- src/component/body/accueil.js
+++ src/component/body/accueil.js
@@ -9,7 +9,11 @@ class Accueil extends Component {
     render() {
         return (
             <div className="Accueil">
-                
+                <div id="degrade">
+                    <h1>Bienvenue sur notre site !</h1>
+                    <p>Vous trouverez ici un large choix d'organisation d'évènements.</p>
+                    <p>Ce site a été réalisé durant l'épreuve web des 24h des iut !</p>
+               </div>
             </div>
         );
     }
