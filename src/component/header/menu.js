4e1d85bf9fde5b13404e8e46f34d13b6dbf2463a
BrightHammer21 <lucieboucquemont@hotmail.fr>
2018-05-26 02:07:34 +0200

    e

--- src/component/header/menu.js
+++ src/component/header/menu.js
@@ -1,12 +1,38 @@
 import React, { Component } from 'react';
+import {Nav} from 'react-bootstrap';
+import {NavItem} from 'react-bootstrap';
 import logo from '../../resource/logo.svg';
 
 class Menu extends Component {
+    handleSelect(selectedKey) {
+        alert(`selected ${selectedKey}`)
+    }
+    
     render() {
-        return (
-            <div className="Menu">
-
-            </div>
+        
+       return (
+            
+            
+                /*<Nav bsStyle="pills"  activeKey={1} onSelect={this.handleSelect}>
+                    <NavItem eventKey={1} href="/home">
+                      Accueil
+                    </NavItem>
+                    <NavItem eventKey={2} href="/home">
+                      Evenements
+                    </NavItem>
+                    <NavItem eventKey={3} href="/home">
+                      Informations
+                    </NavItem>
+                </Nav>*/
+           
+           <ul>
+                <li><a href="#">Accueil</a></li>
+                <li><a href="#">Evenements</a></li>
+                <li><a href="#">Informations</a></li>
+                
+           </ul>
+           
+            
         );
     }
 }
