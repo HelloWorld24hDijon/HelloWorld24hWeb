869150e657006a893353880223ca69a590e190cd
BrightHammer21 <lucieboucquemont@hotmail.fr>
2018-05-26 02:17:40 +0200

    e

--- src/component/header/header.js
+++ src/component/header/header.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from '../../resource/logo.svg';
 
 import Menu from './menu';
+import Accueil from '../body/accueil';
 
 class Header extends Component {
     render() {
@@ -12,6 +13,7 @@ class Header extends Component {
                     <h1 className="App-title">Welcome to React</h1>
                 </header>
                 <Menu />
+                <accueil />
             </div>
         );
     }
